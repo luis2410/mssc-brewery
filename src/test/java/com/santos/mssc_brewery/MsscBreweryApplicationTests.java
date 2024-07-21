68d6b65e9820bff2b51ae1dfa7a4e02409d5510d
Luis fernando Santos navarro <ing.fsantosn@gmail.com>
2024-07-20 19:51:59 -0600

    first commit

---
+++ src/test/java/com/santos/mssc_brewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.mssc_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
